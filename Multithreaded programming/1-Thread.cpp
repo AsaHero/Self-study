332417982f0605b70a3778fe6ce6dbf85f68d2ac
AsaHero <asadbahtiyarov2002@gmail.com>
2021-04-13 21:56:15 +0500

    updeted 1-Thread.cpp

--- Multithreaded programming/1-Thread.cpp
+++ Multithreaded programming/1-Thread.cpp
@@ -3,27 +3,29 @@
 #include <chrono>
 using namespace std;
 
-void DoWork()
+void DoWork(int a, int b)
 {
-	for (size_t i = 0; i < 10; i++)
-	{
-		cout << "Thread id: " << this_thread::get_id() << "\t# " << i << endl;
-		this_thread::sleep_for(chrono::milliseconds(1000));
-	}
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "______ adding process is started ______" << endl;
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << a << " + " << b << " = " << a+b << endl;
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "______ process is ended ______" << endl;
 }
 
 
 int main()
 {
-	thread th1(DoWork);
+	thread th1(DoWork, 5 , 254);
 
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; true; i++)
 	{
-		cout << "Thread id: " << this_thread::get_id() << "\t# " << i << endl;
+		cout << "Thread id: " << this_thread::get_id() << "\tmain"<<"\t# " << i << endl;
 		this_thread::sleep_for(chrono::milliseconds(500));
 	}
 	
 
 	th1.join();
+	
 	return 0;
 }
